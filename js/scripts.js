a3d384383534e64162c36094c90a54dd4f10f589
aroerick <a.roerick@gmail.com>
2018-04-22 19:19:27 -0700

    Added alert prompts for email verification.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,46 @@
 $(function() {
 
+// Carousel
+
     $(".main-carousel").flickity({
         // options
         cellAlign: "left",
         contain: true,
         prevNextButtons: false,
       });
-      
-      $('a[href^="#"]').on('click',function (e) {
+
+// Internal Nav/Smooth Scrolling
+
+    $('a[href^="#"]').on('click',function (e) {
         e.preventDefault();
 
         var target = this.hash;
 
         $('html, body').animate({
-            'scrollTop': $(target).offset().top-84
+            'scrollTop': $(target).offset().top-117
         }, 750, 'swing', function () {
+            });
         });
+
+// Email validation
+
+    $('#email-sub').click(function(e) {
+        e.preventDefault();
+
+        var subEmail = $('#email-text').val();
+        if (validateEmail(subEmail)) {
+            alert("Thanks for subscribing!");
+        } else {
+            alert("Please enter a valid email.");
+        }
     });
-})
+});
+
+function validateEmail(subEmail) {
+    var filter = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    if (filter.test(subEmail)) {
+        return true;
+    } else {
+        return false;
+    }
+}
