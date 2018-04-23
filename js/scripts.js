178194450280289c384945d70eb84ffb9db54b1c
aroerick <a.roerick@gmail.com>
2018-04-22 17:45:01 -0700

    Implemented page navigation and smooth scroll.

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,15 @@ $(function() {
         contain: true,
         prevNextButtons: false,
       });
+      
+      $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
 
-});
+        var target = this.hash;
+
+        $('html, body').animate({
+            'scrollTop': $(target).offset().top-84
+        }, 750, 'swing', function () {
+        });
+    });
+})
